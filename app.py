7d13689e14a10a5ea7fd3fa3871cfb177e470e39
MahmoudSoudah <mahmoudsoudah77@gmail.com>
2026-04-13 13:27:11 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv("data.csv")
+
+X = df[['Age', 'EstimatedSalary']]
+y = df['Purchased']
+
+model = LogisticRegression()
+model.fit(X, y)
+
+st.title("Customer Purchase Prediction")
+
+age = st.number_input("Age")
+salary = st.number_input("Salary")
+
+if st.button("Predict"):
+    result = model.predict([[age, salary]])
+    st.write(result)
+    if result[0]==1:
+        st.success("Customer will Buy")
+    else:
+        st.error("Customer will Not Buy")
