f913f036823102e9cd272de9384d9bf38cab85e8
Mahmoud Soudah <mahmoudsoudah77@gmail.com>
2026-04-19 15:23:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 import streamlit as st
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
+import joblib
 
-df = pd.read_csv("data.csv")
-
-X = df[['Age', 'EstimatedSalary']]
-y = df['Purchased']
-
-model = LogisticRegression()
-model.fit(X, y)
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
 
 st.title("Customer Purchase Prediction")
-
+st.write("Predict whether a customer will purchase based on age and salary.")
 age = st.number_input("Age")
 salary = st.number_input("Salary")
 
 if st.button("Predict"):
-    result = model.predict([[age, salary]])
-  
-    if result[0]==1:
-        st.success("Customer will Buy")
+    if age<=0 or salary <=0:
+        st.error("Please Enter valid positive values.")
+    elif age>100:
+        st.error("Age seems unrealistic")
+    
     else:
-        st.error("Customer will Not Buy")
+        input_data = scaler.transform([[age, salary]])
+        result = model.predict(input_data)
+        prob=model.predict_proba(input_data)
+        st.subheader("Result:")
+    
+        if result[0] == 1:
+            st.success("Customer WILL purchase 🎉")
+        else:
+            st.warning("Customer will NOT purchase ❌")
+        st.subheader("Probability:")
+        st.write("No Purchase(0)",round(prob[0][0]*100,2),"%")
+        st.write("Purchase(1)",round(prob[0][1]*100,2),"%")
